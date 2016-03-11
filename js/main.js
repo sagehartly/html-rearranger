f533aa02834abb51addd445d0d02b72c4bbaec94
sagehartly <sage.hartly@gmail.com>
2016-03-11 17:47:47 -0500

    if

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ var $h1 = $('<h1>');
 $h1.html('Below ground veggies');
 $('body').append($h1);
 
+
+if $above hasClass('above') {
+
+} else {
+
+}
+
 var $h1 = $('<h1>');
 $h1.html('Above ground veggies');
 $('body').append($h1);
